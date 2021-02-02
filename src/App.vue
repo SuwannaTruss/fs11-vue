b0cc09c07e17b3ba698ebc146c5cfe28341e4be4
Truss Suwanna <STruss@Struss-iMac.lan>
2021-02-02 00:02:43 +0000

    added template in Admin page and toggle color for button Admin/user

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,26 @@
 <template>
   <div>
-    <h1>Hello World</h1>
-    <button @click="isAdmin = true">ADMIN</button>
-    <button @click="isAdmin = false">USER</button>
+    <h1>MY PORTFOLIO</h1>
+    <button @click="isAdmin = true" :class="{ 'text-danger': isAdmin }">
+      ADMIN
+    </button>
+    <button @click="isAdmin = false" :class="{ 'text-danger': !isAdmin }">
+      USER
+    </button>
+    <!-- To use Component (3/3: 3. Use component as html tag  -->
     <admin-view v-if="isAdmin" @createProject="addProject" />
     <user-view v-else />
   </div>
 </template>
 
 <script>
+//To use Component (1/3: Import component)
 import AdminView from "./components/AdminView";
 import UserView from "./components/UserView";
 
 export default {
   name: "app",
+  // To use Component (2/3: Declare component)
   components: {
     userView: UserView,
     adminView: AdminView
--- src/components/AdminView.vue
+++ src/components/AdminView.vue
@@ -1,7 +1,21 @@
 <template>
   <div>
-    Title
-    <input v-model="title" />
+    <h2>ADD NEW PROJECT</h2>
+    <!-- use v-model to get input, assign to props -->
+    <div>
+      Project Title
+      <input v-model="title" />
+    </div>
+    <div>
+      Image URL
+      <input v-model="image" />
+    </div>
+    <div>
+      Project Description
+      <input v-model="description" />
+    </div>
+
+    <!-- use @click, is same with 'addEventListener ("click")' -->
     <button @click="handleSubmit">SUBMIT</button>
   </div>
 </template>
@@ -28,4 +42,9 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+* {
+  align-items: center;
+  text-align: center;
+}
+</style>
